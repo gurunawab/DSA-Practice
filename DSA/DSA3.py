290cfc6c79ccc654686b7ea7cbc9136567913a3e
gurunawab <vickysain817@gmail.com>
2026-03-07 09:11:46 +0530

    today progress

--- DSA/DSA3.py
+++ DSA/DSA3.py
@@ -63,4 +63,21 @@ class Solution:
                 
                 start += 1
                 
-        return "" if ans[1] is None else s[ans[1] : ans[2] + 1]         
+        return "" if ans[1] is None else s[ans[1] : ans[2] + 1]      
+
+
+#Dice Throw
+class Solution:
+    def noOfWays(self, m,n,x):
+        
+        dp = [[0] * (x + 1) for _ in range(n + 1)]
+        
+        dp[0][0] = 1
+        
+        for i in range(1, n + 1):
+            for j in range(1, x + 1):
+                for k in range(1, m + 1):
+                    if j - k >= 0:
+                        dp[i][j] += dp[i-1][j-k]
+                        
+        return dp[n][x]  
