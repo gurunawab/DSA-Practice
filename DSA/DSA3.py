f803edfa6d9ff0f5c79fb1d3a262340a6a9b097e
gurunawab <vickysain817@gmail.com>
2026-03-07 13:12:54 +0530

    today progress2

--- DSA/DSA3.py
+++ DSA/DSA3.py
@@ -81,3 +81,47 @@ class Solution:
                         dp[i][j] += dp[i-1][j-k]
                         
         return dp[n][x]  
+
+
+#Minimum Number of flips to Make the Binary String Alternating
+def minFlips(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        s = s + s
+
+        target1 = ""
+        target2 = ""
+        for i in range(len(s)):
+            target1 += "0" if i % 2 == 0 else "1"
+            target2 += "1" if i % 2 == 0 else "0"
+
+        res = len(s)
+        diff1, diff2 = 0, 0
+        l = 0
+
+        for r in range(len(s)):
+
+            if s[r] != target1[r]:
+                diff1 += 1
+
+            if s[r] != target2[r]:
+                diff2 += 1
+
+            if (r - l + 1) > n:
+
+                if s[l] != target1[l]:
+                    diff1 -= 1
+
+                if s[l] != target2[l]:
+                    diff2 -= 1
+
+                l += 1
+
+            if (r - l + 1) == n:
+                res = min(res, diff1, diff2)
+
+        return res                                
+        
