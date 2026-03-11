5a166bfae0425ba40d598bcf25f05c053766677e
gurunawab <vickysain817@gmail.com>
2026-03-11 14:54:25 +0530

    Today Progress

--- DSA/DSA5.py
+++ DSA/DSA5.py
@@ -17,3 +17,35 @@ def countSubarrays(self, arr):
             ans += (nse[i] - i)
             
         return ans 
+
+
+#Sum of subarray minimums
+def sumSubMins(self, arr):
+        n = len(arr)
+        left = [-1] * n
+        right = [n] * n
+        stack = []
+        
+        for i in range(n):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            if stack:
+                left[i] = stack[-1]
+            stack.append(i)
+            
+        stack = []    
+            
+        for i in range(n - 1, -1, -1):
+            while stack and arr[stack[-1]] > arr[i]:
+                stack.pop()
+            if stack:
+                right[i] = stack[-1]
+            stack.append(i)
+            
+        total_sum = 0
+        for i in range(n):
+            count = (i - left[i]) * (right[i] - i)
+            total_sum += count * arr[i]
+            
+        return total_sum    
+        
