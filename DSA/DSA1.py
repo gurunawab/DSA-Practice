e1c73934a1da04d5b81022b356dea42d290f4a01
gurunawab <vickysain817@gmail.com>
2026-03-24 13:48:31 +0530

    Today Progress

--- DSA/DSA1.py
+++ DSA/DSA1.py
@@ -52,3 +52,56 @@ if __name__ == "__main__":
   res = sieve(n)
   for ele in res:
       print(ele, end=' ')     
+
+
+#Construct Product Matrix
+def constructProductMatrix(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        n = len(grid)
+        m = len(grid[0])
+        MOD = 12345
+        
+        p = [[1 for _ in range(m)] for _ in range(n)]
+
+        prefix = 1
+        for i in range(n):
+            for j in range(m):
+                p[i][j] = prefix
+                prefix = (prefix * grid[i][j]) % MOD
+
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                p[i][j] = (p[i][j] * suffix) % MOD
+                suffix = (suffix * grid[i][j]) % MOD
+
+        return p 
+
+
+#Course Schedule I
+def canFinish(self, n, prerequisites):
+        adj = [[] for _ in range(n)]
+        indegree = [0] * n
+        
+        for dest, src in prerequisites:
+            adj[src].append(dest)
+            indegree[dest] += 1
+            
+        queue = deque([i for i in range(n) if indegree[i] == 0])
+        
+        count = 0
+        
+        while queue:
+            node = queue.popleft()
+            count += 1
+            
+            for neighbor in adj[node]:
+                indegree[neighbor] -= 1
+                
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+                    
+        return count == n 
