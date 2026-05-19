ab572169866ffd467787b08138e449a6d4d5c44d
gurunawab <vickysain817@gmail.com>
2026-05-19 21:36:52 +0530

    Today Progress

--- DSA/DSA Easy1.py
+++ DSA/DSA Easy1.py
@@ -233,6 +233,22 @@ def maxSum(self, n: int) -> int:
             return memo[num]
             
         return solve(n)    
-                
+
+#Minimum Common Value
+def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i = 0
+        j = 0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+
+            elif nums1[i] < nums2[j]:
+                i += 1
+
+            else:
+                j += 1
+
+        return -1                 
 
 
--- DSA/DSA9.py
+++ DSA/DSA9.py
@@ -30,3 +30,34 @@ class Solution:
                         visited.add(nxt)
                         queue.append(nxt)
             steps += 1  
+
+#Minimum Multiplications to reach End
+from collections import deque
+
+class Solution:
+    def minSteps(self, arr, start, end):
+        if start == end:
+            return 0
+            
+        MOD = 1000
+        
+        dist = [-1] * MOD
+        
+        queue = deque([start])
+        dist[start] = 0
+        
+        while queue:
+            node = queue.popleft()
+            
+            for num in arr:
+                next_node = (node * num) % MOD
+                
+                if dist[next_node] == -1:
+                    dist[next_node] = dist[node] + 1
+                    
+                    if next_node == end:
+                        return dist[next_node]
+                        
+                    queue.append(next_node)
+                    
+        return -1                
