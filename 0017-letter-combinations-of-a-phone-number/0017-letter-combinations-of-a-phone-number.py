06efc1c1f444f046292fe05cc2662e6254ec5e8a
Uday Sen <vickysain817@gmail.com>
2026-08-14 09:01:05 +0530

    Time: 0 ms (100%), Space: 19.2 MB (95.7%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,7 @@
+from itertools import product
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        m = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        return [''.join(p) for p in product(*(m[d] for d in digits))]
